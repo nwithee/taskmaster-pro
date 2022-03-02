68bb9834290973281437d8046d4424add8ae8c49
nzwithee@gmail.com <Cartoon#01>
2022-03-01 21:50:16 -0600

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -137,6 +137,69 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+  },
+  deactivate: function(event) {
+  },
+  over: function(event) {
+  },
+  out: function(event) {
+  },
+  
+  
+  
+  update: function(event) {
+    var tempArr =[];
+
+    $(this).children().each(function() {
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date =$(this)
+      .find("span")
+      .text()
+      .trim();
+      
+      tempArr.push({
+        text: text,
+        date: date,
+      });
+    });
+    var arrName = $(this)
+        .attr("id")
+        .replace("list-", "");
+
+      tasks[arrName] = tempArr;
+      saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
+
+
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
